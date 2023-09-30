c12af94299190b8aab6b33570f5f6773e5d716f1
jimmy <475772164@qq.com>
2023-09-30 15:38:05 +0800

    fix: 修改样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,13 @@
+import { Line, LineConfig, Pie, PieConfig } from '@ant-design/plots';
 import { GridContent, ProCard, StatisticCard } from '@ant-design/pro-components';
-import { Col, Row, theme, Divider, Image, Typography } from 'antd';
+import { Col, Divider, Image, Row, Typography } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { Line, Pie, LineConfig, PieConfig } from '@ant-design/plots';
 
 const { Statistic } = StatisticCard;
 const { Text, Title } = Typography;
 
 const DemoLine = () => {
   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    asyncFetch();
-  }, []);
 
   const asyncFetch = () => {
     fetch('/chart/line/visit')
@@ -21,6 +17,11 @@ const DemoLine = () => {
         console.log('fetch data failed', error);
       });
   };
+
+  useEffect(() => {
+    asyncFetch();
+  }, []);
+
   const config: LineConfig = {
     data,
     padding: 'auto',
@@ -88,19 +89,25 @@ const DemoPie = () => {
 };
 
 const Home: React.FC = () => {
-  const { token } = theme.useToken();
-  // const { initialState } = useModel('@@initialState');
   return (
-    <GridContent>
+    <GridContent contentWidth="Fixed">
       <Row gutter={[24, 24]}>
         <Col span={24}>
-          <ProCard style={{ backgroundColor: token['blue-6'] }}>
+          <ProCard style={{ backgroundColor: '#1890ff' }}>
             <br />
-            <Title style={{ color: token.colorWhite }} level={4}>欢迎使用Letga !</Title>
-            <Text style={{ color: token.colorWhite }}>Letga 是基于 GoFrame + AntDesign 的中后台管理系统，系统集成了通用的中后台基础功能模块，是一款高性能、易扩展、高颜值的企业级开源系统。</Text>
+            <Title style={{ color: '#ffffff' }} level={4}>
+              欢迎使用Letga !
+            </Title>
+            <Text style={{ color: '#ffffff' }}>
+              Letga 是基于 GoFrame + AntDesign
+              的中后台管理系统，系统集成了通用的中后台基础功能模块，是一款高性能、易扩展、高颜值的企业级开源系统。
+            </Text>
             <br />
-            <Text style={{ color: token.colorWhite }}>在使用中遇到问题，可以查看「使用帮助」，或者在「Letga社区」寻求帮助</Text>
-            <br /><br />
+            <Text style={{ color: '#ffffff' }}>
+              在使用中遇到问题，可以查看「使用帮助」，或者在「Letga社区」寻求帮助
+            </Text>
+            <br />
+            <br />
           </ProCard>
         </Col>
         <Col span={24}>
@@ -112,39 +119,27 @@ const Home: React.FC = () => {
               statistic={{
                 title: '昨日请求数',
                 value: 234,
-                description: (
-                  <Statistic
-                    title="较本月平均"
-                    value="8.04%"
-                    trend="down"
-                  />
-                ),
+                description: <Statistic title="较本月平均" value="8.04%" trend="down" />,
               }}
             />
             <StatisticCard
               statistic={{
                 title: '近7天请求',
                 value: 234,
-                description: (
-                  <Statistic title="较近月平均" value="8.04%" trend="up" />
-                ),
+                description: <Statistic title="较近月平均" value="8.04%" trend="up" />,
               }}
             />
             <StatisticCard
               statistic={{
                 title: '本月请求数',
                 value: 234,
-                description: (
-                  <Statistic title="较上月平均" value="8.04%" trend="up" />
-                ),
+                description: <Statistic title="较上月平均" value="8.04%" trend="up" />,
               }}
             />
           </ProCard>
         </Col>
         <Col span={16}>
-          <ProCard title="流量统计 - demo"
-            headerBordered
-          >
+          <ProCard title="流量统计 - demo" headerBordered>
             <DemoLine />
           </ProCard>
         </Col>
@@ -157,10 +152,10 @@ const Home: React.FC = () => {
           </ProCard>
         </Col>
         <Col span={16}>
-          <ProCard title="用户分布 - demo"
-            headerBordered
-          >
-            <div style={{ width: '100%', height: 400, textAlign: 'center' }}><Image src="/map_china.png" height={400} /></div>
+          <ProCard title="用户分布 - demo" headerBordered>
+            <div style={{ width: '100%', height: 400, textAlign: 'center' }}>
+              <Image src="/map_china.png" height={400} />
+            </div>
           </ProCard>
         </Col>
         <Col span={24}>
@@ -168,89 +163,7 @@ const Home: React.FC = () => {
         </Col>
       </Row>
     </GridContent>
-  )
-  // return (
-  //   <PageContainer
-  //     prefixCls='prefix'
-  //     pageHeaderRender={() => <></>}
-  //   >
-  //     <Row gutter={[24, 24]}>
-  //       <Col span={24}>
-  //         <ProCard style={{ backgroundColor: token['blue-6'] }}>
-  //           <br/>
-  //           <Title style={{ color: token.colorWhite }} level={4}>欢迎使用Letga !</Title>
-  //           <Text style={{ color: token.colorWhite }}>Letga 是基于 GoFrame + AntDesign 的中后台管理系统，系统集成了通用的中后台基础功能模块，是一款高性能、易扩展、高颜值的企业级开源系统。</Text>
-  //           <br/>
-  //           <Text style={{ color: token.colorWhite }}>在使用中遇到问题，可以查看「使用帮助」，或者在「Letga社区」寻求帮助</Text>
-  //           <br/><br/>
-  //         </ProCard>
-  //       </Col>
-  //       <Col span={24}>
-  //         <Divider></Divider>
-  //       </Col>
-  //       <Col span={8}>
-  //         <ProCard gutter={8} direction="column" split="horizontal">
-  //           <StatisticCard
-  //             statistic={{
-  //               title: '昨日请求数',
-  //               value: 234,
-  //               description: (
-  //                 <Statistic
-  //                   title="较本月平均"
-  //                   value="8.04%"
-  //                   trend="down"
-  //                 />
-  //               ),
-  //             }}
-  //           />
-  //           <StatisticCard
-  //             statistic={{
-  //               title: '近7天请求',
-  //               value: 234,
-  //               description: (
-  //                 <Statistic title="较近月平均" value="8.04%" trend="up" />
-  //               ),
-  //             }}
-  //           />
-  //           <StatisticCard
-  //             statistic={{
-  //               title: '本月请求数',
-  //               value: 234,
-  //               description: (
-  //                 <Statistic title="较上月平均" value="8.04%" trend="up" />
-  //               ),
-  //             }}
-  //           />
-  //         </ProCard>
-  //       </Col>
-  //       <Col span={16}>
-  //         <ProCard title="流量统计 - demo"
-  //           headerBordered
-  //         >
-  //           <DemoLine />
-  //         </ProCard>
-  //       </Col>
-  //       <Col span={24}>
-  //         <Divider></Divider>
-  //       </Col>
-  //       <Col span={8}>
-  //         <ProCard title="客户端 - demo" headerBordered>
-  //           <DemoPie />
-  //         </ProCard>
-  //       </Col>
-  //       <Col span={16}>
-  //         <ProCard title="用户分布 - demo"
-  //           headerBordered
-  //         >
-  //           <div style={{width:'100%', height: 400, textAlign: 'center'}}><Image src="/map_china.png" height={400} /></div>
-  //         </ProCard>
-  //       </Col>
-  //       <Col span={24}>
-  //         <Divider></Divider>
-  //       </Col>
-  //     </Row>
-  //   </PageContainer>
-  // );
+  );
 };
 
 export default Home;
